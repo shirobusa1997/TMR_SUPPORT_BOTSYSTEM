fd8c08b1f59e617f04f410c89a8f2fbf0f7b9c68
MOJATTO STUDIO <daichiandroid1997@gmail.com>
2018-07-19 17:38:10 +0900

    Adapt to new [TMR] cloud instance.

--- tmr_spbotprog.py
+++ tmr_spbotprog.py
@@ -1,45 +1,47 @@
 import discord
 import asyncio
 
+import ID_Information
+
 client = discord.Client()
 
-@client.event
-async def on_ready():
+@asyncio.coroutine
+def on_ready():
 	print('Logged in as')
 	print(client.user.name)
 	print(client.user.id)
 	print('------')
 
-@client.event
-async def on_message(message):
+@asyncio.coroutine
+def on_message(message):
 	if message.content.startswith('!test'):
 		counter = 0
-		tmp = await client.send_message(message.channel, 'Calculating messages...')
-		async for log in client.logs_from(message.channel, limit = 100):
+		tmp = yield from client.send_message(message.channel, 'Calculating messages...')
+		for log in client.logs_from(message.channel, limit = 100):
 			if log.author == message.author:
 				counter += 1
 
-		await client.edit_message(tmp, 'You have {} messages.'.format(counter))
+		yield from client.edit_message(tmp, 'You have {} messages.'.format(counter))
 	elif message.content.startswith('!sleep'):
-		await asyncio.sleep(5)
-		await client.send_message(message.channel, 'Done sleeping')
+		yield from asyncio.sleep(5)
+		yield from client.send_message(message.channel, 'Done sleeping')
 
 	elif message.content.startswith('!greeting'):
-		await client.send_message(message.channel, 'ほあようごぁいまーしゅ！')
-		await client.send_message(message.channel, 'ほんほはほあようごぁいましゅといいたかったんしゅが、かちゅぜちゅがわるいためほあようごぁいまーしゅになってちまいまいた!')
+		yield from client.send_message(message.channel, 'ほあようごぁいまーしゅ！')
+		yield from client.send_message(message.channel, 'ほんほはほあようごぁいましゅといいたかったんしゅが、かちゅぜちゅがわるいためほあようごぁいまーしゅになってちまいまいた!')
 	elif message.content.startswith('!hw'):
-		await client.send_message(message.channel, 'Hello World.')
+		yield from client.send_message(message.channel, 'Hello World.')
 
 	elif message.content.startswith('タンヤオ') or message.content.startswith('断么九'):
-		await client.send_file(client.get_server('<REPLACE TO TARGET SERVER ID>').default_channel, 'Resources/tanyao.png')
+		yield from client.send_file(client.get_server(SERVER_ID).default_channel, 'Resources/tanyao.png')
 
 	elif message.content.startswith('!clanmatch'):
 		counter = 0
 		tmp = awai client.send_message(messafe.channel, 'Let\'s DICE !!!')
-		async for log in client.logs_from(messafe.channel, limit=100)
+		for log in client.logs_from(messafe.channel, limit=100)
 			if log.author == message.author:
 				counter += 1
 
-			await client.edit_message(tmp, 'You have {} messages.'. format(counter))
+			yield from client.edit_message(tmp, 'You have {} messages.'. format(counter))
 			
-client.run('<REPLACE TO BOT TOKEN>')
+client.run(BOT_TOKEN)
